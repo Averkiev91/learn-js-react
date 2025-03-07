5803171c01ed78ae059c02782ad982d859b25baa
maks <i@skr1n.ru>
2025-03-07 23:48:01 +0300

    done hw-2

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,27 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { restaurants } from '../materials/mock.js';
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import { createRoot } from 'react-dom/client';
+
+const root = document.getElementById('root');
+
+const reactRoot = createRoot(root);
+
+reactRoot.render(
+  <div>
+    {restaurants.map((element) => (
+      <div key={element.id}>
+        <h3>{'Ресторан: ' + element.name}</h3>
+        <p>Меню</p>
+        <ul>
+          <li>{element.menu.map((item) => item.name)}</li>
+        </ul>
+        <h3>Отзывы</h3>
+        <ul>
+          {element.reviews.map((review) => (
+            <li key={review.id}>{review.text}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
+  </div>
+);
