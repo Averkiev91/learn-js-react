59548e61c8f788fa4e7d8e73eda402df071e30c5
maks <i@skr1n.ru>
2025-03-08 00:12:16 +0300

    del div

--- src/main.jsx
+++ src/main.jsx
@@ -8,24 +8,20 @@ const reactRoot = createRoot(root);
 
 reactRoot.render(
   <>
-    <div>
-      {restaurants.map((element) => (
-        <div key={element.id}>
-          <h3>{'Ресторан: ' + element.name}</h3>
-          <p>Меню</p>
-          <ul>
-            <li key={element.menu.map((item) => item.id)}>
-              {element.menu.map((item) => item.name)}
-            </li>
-          </ul>
-          <h3>Отзывы</h3>
-          <ul>
-            {element.reviews.map((review) => (
-              <li key={review.id}>{review.text}</li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </div>
+    {restaurants.map((element) => (
+      <div key={element.id}>
+        <h3>{'Ресторан: ' + element.name}</h3>
+        <p>Меню</p>
+        <ul>
+          <li key={element.menu.map((item) => item.id)}>{element.menu.map((item) => item.name)}</li>
+        </ul>
+        <h3>Отзывы</h3>
+        <ul>
+          {element.reviews.map((review) => (
+            <li key={review.id}>{review.text}</li>
+          ))}
+        </ul>
+      </div>
+    ))}
   </>
 );
