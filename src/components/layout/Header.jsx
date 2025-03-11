d9357235483158d708fa7e21421ce30b5cb05ace
Maksim Averkiev <amd3500m@gmail.com>
2025-03-11 16:26:15 +0300

    component header

---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <h1>Learn React</h1>
+        <nav>
+          <a href='/'>Главная</a>
+        </nav>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
