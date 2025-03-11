8f66f5f26e2ae6d035652cec64128f75746c491e
Maksim Averkiev <amd3500m@gmail.com>
2025-03-11 18:08:28 +0300

    add Counter

--- src/components/restaurant/Menu.jsx
+++ src/components/restaurant/Menu.jsx
@@ -1,10 +1,16 @@
+import React from 'react';
+import Counter from './Counter';
+
 const Menu = ({ menu }) => {
   return (
-    <ul>
-      <li>{`Name: ${menu.name}`}</li>
-      <li>{`Price: ${menu.price}`}</li>
-      <li>{`Ingredients: ${menu.ingredients}`}</li>
-    </ul>
+    <div>
+      <ul>
+        <li>{`Name: ${menu.name}`}</li>
+        <li>{`Price: ${menu.price}`}</li>
+        <li>{`Ingredients: ${menu.ingredients}`}</li>
+      </ul>
+      <Counter />
+    </div>
   );
 };
 
