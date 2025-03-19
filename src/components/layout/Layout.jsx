49453e28fbb150ee4448eb151ae0b50842fb0faa
Maksim Averkiev <amd3500m@gmail.com>
2025-03-19 13:32:45 +0300

    added a component to Layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from './Header';
 import Footer from './Footer';
+import ProgressBar from '../ProgressBar/ProgressBar';
 
 const Layout = ({ title, children }) => {
   return (
     <div>
+      <ProgressBar />
       <Header />
       <h1>{title}</h1>
       <main>{children}</main>
