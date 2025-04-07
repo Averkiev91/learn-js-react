c2c847c39b4774fdb05fa747d07904ea76d137bb
maks <i@skr1n.ru>
2025-04-07 11:03:00 +0300

    added display of the AddReview component for authorized users in the Reviews component

---
+++ src/components/Review/AddReview.jsx
@@ -0,0 +1,55 @@
+import React, { useState } from 'react';
+import { useAddReviewMutation } from '../../redux/services/api';
+import { useParams } from 'react-router';
+import Counter from '../Counter/Counter';
+
+const AddReview = ({ userId }) => {
+  const { restaurantId } = useParams();
+  const [addReview, { isLoading }] = useAddReviewMutation();
+
+  const [text, setText] = useState('');
+  const [rating, setRating] = useState(5);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!text.trim()) return;
+
+    await addReview({
+      restaurantId,
+      review: {
+        userId,
+        text,
+        rating: Number(rating),
+      },
+    });
+    setText('');
+    setRating(5);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <textarea
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+          placeholder='Напишите отзыв'
+          required
+        />
+      </div>
+
+      <div>
+        <label>
+          Рейтинг:
+          <Counter value={rating} onChange={setRating} />
+        </label>
+      </div>
+
+      <button type='submit' disabled={isLoading}>
+        {isLoading ? 'Загрузка...' : 'Добавить отзыв'}
+      </button>
+    </form>
+  );
+};
+
+export default AddReview;
--- src/components/Reviews/Reviews.jsx
+++ src/components/Reviews/Reviews.jsx
@@ -1,12 +1,19 @@
+import React from 'react';
+import { useUser } from '../../redux/hooks/useUser';
 import ReviewItemsContainer from '../Review/ReviewsContainer';
+import AddReview from '../Review/AddReview';
 
 const Reviews = ({ reviews }) => {
+  const { user } = useUser();
+
   return (
     <div>
       <h3>Отзывы</h3>
       {reviews.map((review) => (
         <ReviewItemsContainer key={review.id} review={review} />
       ))}
+
+      {user && <AddReview userId={user.id} />}
     </div>
   );
 };
