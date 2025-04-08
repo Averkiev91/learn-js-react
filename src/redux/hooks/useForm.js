34d166e9f563ce18bfa9d06e8461104a9618dd28
Maksim Averkiev <amd3500m@gmail.com>
2025-04-08 11:01:31 +0300

    useReducer

--- src/redux/hooks/useForm.js
+++ src/redux/hooks/useForm.js
@@ -1,24 +1,66 @@
-import { useState, useCallback } from 'react';
+import { useReducer, useCallback } from 'react';
+
+const initialState = {
+  text: '',
+  rating: 5,
+};
+
+const actionTypes = {
+  SET_TEXT: 'SET_TEXT',
+  SET_RATING: 'SET_RATING',
+  RESET: 'RESET',
+};
+
+const formReducer = (state, action) => {
+  switch (action.type) {
+    case actionTypes.SET_TEXT:
+      return {
+        ...state,
+        text: action.payload,
+      };
+    case actionTypes.SET_RATING:
+      return {
+        ...state,
+        rating: action.payload,
+      };
+    case actionTypes.RESET:
+      return {
+        text: action.payload.text || initialState.text,
+        rating: action.payload.rating || initialState.rating,
+      };
+    default:
+      return state;
+  }
+};
 
 export const useForm = (initialData = {}) => {
   const { initialText = '', initialRating = 5 } = initialData;
 
-  const [text, setText] = useState(initialText);
-  const [rating, setRating] = useState(initialRating);
+  const [form, dispatch] = useReducer(formReducer, {
+    text: initialText,
+    rating: initialRating,
+  });
+
+  const setText = useCallback((text) => {
+    dispatch({ type: actionTypes.SET_TEXT, payload: text });
+  }, []);
+
+  const setRating = useCallback((rating) => {
+    dispatch({ type: actionTypes.SET_RATING, payload: rating });
+  }, []);
 
   const reset = useCallback(
     (newText = initialText, newRating = initialRating) => {
-      setText(newText);
-      setRating(newRating);
+      dispatch({
+        type: actionTypes.RESET,
+        payload: { text: newText, rating: newRating },
+      });
     },
     [initialText, initialRating]
   );
 
   return {
-    form: {
-      text,
-      rating,
-    },
+    form,
     setText,
     setRating,
     reset,
