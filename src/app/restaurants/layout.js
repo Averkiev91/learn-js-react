025994e4a4ec7f9264b322eda0597cbbb69b3ddc
Maksim Averkiev <amd3500m@gmail.com>
2025-04-10 16:25:35 +0300

    Simplified LayoutRestaurantPage component

--- src/app/restaurants/layout.js
+++ src/app/restaurants/layout.js
@@ -1,34 +1,9 @@
 "use client";
 
-import RestaurantNavigation from "../../components/RestaurantNavigation/RestaurantNavigation";
-import { useGetRestaurantsQuery } from "../../redux/services/api";
-import LoadErrorDisplay from "../../components/LoadErrorDisplay/LoadErrorDisplay";
+import RestaurantLayoutClient from "../../components/RestaurantLayoutClient/RestaurantLayoutClient";
 
 const LayoutRestaurantPage = ({ children }) => {
-  const { data: restaurants, isLoading, isError } = useGetRestaurantsQuery();
-
-  return (
-    <LoadErrorDisplay
-      data={restaurants}
-      isLoading={isLoading}
-      isError={isError}
-    >
-      {restaurants && (
-        <>
-          <nav style={{ display: "flex", gap: "10px" }}>
-            {restaurants.map((restaurant) => (
-              <RestaurantNavigation
-                key={restaurant.id}
-                restaurant={restaurant}
-              />
-            ))}
-            {restaurants.length === 0 && <p>Ресторанов нет</p>}
-          </nav>
-          <main>{children}</main>
-        </>
-      )}
-    </LoadErrorDisplay>
-  );
+  return <RestaurantLayoutClient>{children}</RestaurantLayoutClient>;
 };
 
 export default LayoutRestaurantPage;
