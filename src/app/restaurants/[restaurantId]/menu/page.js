729218862e7c5c67e257e829971780c8491faa61
Maksim Averkiev <amd3500m@gmail.com>
2025-04-10 16:23:02 +0300

    Updated MenuPage and ReviewsPage components

--- src/app/restaurants/[restaurantId]/menu/page.js
+++ src/app/restaurants/[restaurantId]/menu/page.js
@@ -1,36 +1,9 @@
-"use client";
+import MenuPageClient from "../../../../components/MenuPageClient/MenuPageClient";
 
-import { useParams } from "next/navigation";
-import Menu from '../../../../components/Menu/Menu';
-import { useGetDishesByRestaurantIdQuery } from '../../../../redux/services/api';
+const MenuPage = ({ params }) => {
+  const { restaurantId } = params;
 
-const MenuPage = () => {
-  const { restaurantId } = useParams();
-  const { data: menu, isLoading, isError } = useGetDishesByRestaurantIdQuery(restaurantId);
-
-  if (isLoading) {
-    return (
-      <div>
-        <p>Загрузка</p>
-      </div>
-    );
-  }
-
-  if (isError) {
-    return (
-      <div>
-        <p>Ошибка</p>
-      </div>
-    );
-  }
-
-  return menu?.length > 0 ? (
-    <Menu menu={menu} />
-  ) : (
-    <div>
-      <p>Меню пока не добавлено</p>
-    </div>
-  );
+  return <MenuPageClient restaurantId={restaurantId} />;
 };
 
 export default MenuPage;
