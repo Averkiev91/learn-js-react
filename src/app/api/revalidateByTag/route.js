84c502dcaa8c696753cb3e2fd79ee1745664c218
maks <i@skr1n.ru>
2025-04-14 17:11:47 +0300

    Добавлен новый маршрут для повторной валидации тегов в API

---
+++ src/app/api/revalidateByTag/route.js
@@ -0,0 +1,10 @@
+import { NextResponse } from 'next/server';
+import { revalidateTag } from 'next/cache';
+
+export async function GET(request) {
+  const tag = request.nextUrl.searchParams.get('tag');
+
+  revalidateTag(tag);
+
+  return NextResponse.json({ status: 'success', tag });
+}
