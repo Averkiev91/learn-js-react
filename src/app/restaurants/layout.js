8e9e19b1be66745f91d402b1053ea7b642ebd5ef
maks <i@skr1n.ru>
2025-04-14 21:02:03 +0300

    Updated RestaurantLayout component: added asynchronous call getRestaurants to get restaurant data, added Suspense component to handle loading. Changed RestaurantLayoutClient component to receive restaurant data via props

--- src/app/restaurants/layout.js
+++ src/app/restaurants/layout.js
@@ -1,3 +1,14 @@
 import RestaurantLayoutClient from '../../components/RestaurantLayoutClient/RestaurantLayoutClient';
+import { Suspense } from 'react';
+import { getRestaurants } from '../../services/getRestaurants';
 
-export default RestaurantLayoutClient;
+const RestaurantsLayout = async ({ children }) => {
+  const restaurants = await getRestaurants();
+  return (
+    <Suspense fallback={<div>Загрузка ресторанов...</div>}>
+      <RestaurantLayoutClient restaurants={restaurants}>{children}</RestaurantLayoutClient>
+    </Suspense>
+  );
+};
+
+export default RestaurantsLayout;
--- src/components/RestaurantLayoutClient/RestaurantLayoutClient.jsx
+++ src/components/RestaurantLayoutClient/RestaurantLayoutClient.jsx
@@ -1,27 +1,19 @@
 'use client';
 
 import React from 'react';
-import { useGetRestaurantsQuery } from '../../redux/services/api';
-import LoadErrorDisplay from '../LoadErrorDisplay/LoadErrorDisplay';
 import RestaurantNavigation from '../RestaurantNavigation/RestaurantNavigation';
 
-const RestaurantLayoutClient = ({ children }) => {
-  const { data: restaurants, isLoading, isError } = useGetRestaurantsQuery();
-
+const RestaurantLayoutClient = ({ children, restaurants }) => {
   return (
-    <LoadErrorDisplay data={restaurants} isLoading={isLoading} isError={isError}>
-      {restaurants && (
-        <>
-          <nav style={{ display: 'flex', gap: '10px' }}>
-            {restaurants.map((restaurant) => (
-              <RestaurantNavigation key={restaurant.id} restaurant={restaurant} />
-            ))}
-            {restaurants.length === 0 && <p>Ресторанов нет</p>}
-          </nav>
-          <main>{children}</main>
-        </>
-      )}
-    </LoadErrorDisplay>
+    <>
+      <nav style={{ display: 'flex', gap: '10px' }}>
+        {restaurants.map((restaurant) => (
+          <RestaurantNavigation key={restaurant.id} restaurant={restaurant} />
+        ))}
+        {restaurants.length === 0 && <p>Ресторанов нет</p>}
+      </nav>
+      <main>{children}</main>
+    </>
   );
 };
 
